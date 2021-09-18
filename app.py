4f10f34c39d3ad30fb14a0ef13a86766c0e9ef6f
Albert Wan <albert@alberts-air.dyndns.rice.edu>
2021-09-17 23:23:37 -0500

    M

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import session
+from flask import redirect
+from flask import url_for
+import os
+import sqlite3
 
+
+app = Flask(__name__)
+create()
+# has logout button to log out
+@app.route("/homepage")
+def homepage():
+    return render_template("homepage.html")
+
+
+# login page with form which sends request to /auth route
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+# removes session data for username
+@app.route("/createAccount")
+def createAccount():
+    return render_template("createAccount.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
